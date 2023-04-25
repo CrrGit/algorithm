b95deac49e507fd9fcc4d4ebff030c97fd0a6ab9
CrrGit <130305603+CrrGit@users.noreply.github.com>
2023-04-25 15:03:43 +0900

    [Bronze V] Title: A/B, Time: 228 ms, Memory: 18028 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.java
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -1,8 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOError;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		double a, b;
+	public static void main(String[] args) throws IOException{
+        
+        double a, b;
+        
+		// 1번째 방법
 		Scanner sc = new Scanner(System.in);
 
 		do {
@@ -12,6 +20,18 @@ public class Main {
 		
 		sc.close();
 		System.out.println(a / b);
+
+		// 2번째 방법
+		/* 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+ 		String str = br.readLine();
+		StringTokenizer st = new StringTokenizer(str," ");
+		a = Double.parseDouble(st.nextToken());
+		b = Double.parseDouble(st.nextToken());
+		
+		System.out.println(a/b); 
+		*/
 
     }
 }
