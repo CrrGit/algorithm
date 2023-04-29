826454657682751f421428118a9ffda1ec54d3c1
CrrGit <130305603+CrrGit@users.noreply.github.com>
2023-04-29 23:23:08 +0900

    [Bronze V] Title: A+B - 7, Time: 256 ms, Memory: 18660 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int x, a, b = 0;
+        
+        Scanner sc = new Scanner(System.in);
+        x = sc.nextInt();
+
+        for(int i = 1; i <= x; i++) {
+            a = sc.nextInt();
+            b = sc.nextInt();
+            System.out.println("Case #" + i + ": " + (a + b));
+        }
+		sc.close();
+	}
+
+}
