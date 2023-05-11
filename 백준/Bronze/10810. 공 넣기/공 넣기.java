b33d7535db05e8261b8f2a4d0db9c5522f674b11
CrrGit <130305603+CrrGit@users.noreply.github.com>
2023-05-11 17:42:11 +0900

    [Bronze III] Title: 공 넣기, Time: 252 ms, Memory: 19084 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.java
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -1,25 +1,24 @@
-import java.util.*;
-public class Main {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
+public class Main {
+ 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
-		int[] arr = new int[num];
-		int line = sc.nextInt();
-		for(int i=0;i<line;i++)
-		{
-			int st = sc.nextInt();
+		int []arr = new int[sc.nextInt()];
+		int a = sc.nextInt();
+
+		for (int i = 0; i < a; i++) {
+			int stt = sc.nextInt();
 			int end = sc.nextInt();
-			int ballnum = sc.nextInt();
-			for(int j = st-1;j<end;j++)
-			{
-				arr[j] = ballnum;
+			int num = sc.nextInt();
+			for(int j = stt - 1; j < end; j++) {
+				arr[j] = num;
 			}
 		}
-		for(int i=0;i<arr.length;i++)
-		{
+		sc.close();
+
+		for(int i=0; i < arr.length; i++) {
 			System.out.print(arr[i] + " ");
 		}
 		
 	}
-	}
+}
