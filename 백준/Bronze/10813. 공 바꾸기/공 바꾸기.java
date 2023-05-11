3f5c00c5ada4ca80256995e20cbe64ddd028b541
CrrGit <130305603+CrrGit@users.noreply.github.com>
2023-05-11 17:59:44 +0900

    [Bronze II] Title: 공 바꾸기, Time: 244 ms, Memory: 18576 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -1,19 +1,28 @@
-import java.util.*;
- 
+import java.util.Scanner;
+
 public class Main {
-	public static void main(String[] args){
+ 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n=sc.nextInt(), m=sc.nextInt();
-		int [] bag = new int[n+1];
-		for(int i=1;i<=n;i++)
-			bag[i]=i;
-		for(int i=0;i<m;i++) {
-			int a=sc.nextInt(), b=sc.nextInt();
-			int temp=bag[a];
-			bag[a]=bag[b];
-			bag[b]=temp;
+		int []arr = new int[sc.nextInt()];
+		int a = sc.nextInt();
+
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = i + 1;
 		}
-		for(int i=1;i<=n;i++)
-			System.out.print(bag[i]+ " ");
+
+		for (int i = 0; i < a; i++) {
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+
+			int tmp = arr[x - 1];
+			arr[x - 1] = arr[y - 1];
+			arr[y - 1] = tmp;
+		}
+		sc.close();
+
+		for(int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		
 	}
 }
