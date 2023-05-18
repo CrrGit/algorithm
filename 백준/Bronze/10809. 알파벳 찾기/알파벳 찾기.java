45e0a881eb224a5727befa522e327b201cf3e8f7
CrrGit <130305603+CrrGit@users.noreply.github.com>
2023-05-18 17:41:55 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 228 ms, Memory: 18312 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -4,10 +4,11 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String str = sc.nextLine();
+		char ch;
 		sc.close();
 
 		for (int i = 0; i < 26; i++) {
-			char ch = (char) (i + 97);
+			ch = (char) (i + 97);
 			System.out.print(str.indexOf(ch) + " ");
 		}
 	}
